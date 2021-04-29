7584e9405822a8c4cfcecd0e0cc3c41113155835
Fedor <funtik21975@gmail.com>
2021-04-29 21:59:13 +0300

    First commit

---
+++ src/Configurator/ConsoleCommandConfiguratorSimple.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Prokl\DbCommands\Configurator;
+
+use Exception;
+use IteratorAggregate;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class ConsoleCommandConfiguratorSimple
+ * @package Local\Services\Console
+ *
+ * @since 10.12.2020
+ * @since 24.12.2020 Рефакторинг.
+ */
+class ConsoleCommandConfiguratorSimple
+{
+    /**
+     * @var Application $application Конфигуратор консольных команд.
+     */
+    private $application;
+
+    /**
+     * @var Command[] $commands Команды.
+     */
+    private $commands;
+
+    /**
+     * ConsoleCommandConfigurator constructor.
+     *
+     * @param Application $application Конфигуратор консольных команд.
+     * @param Command     ...$commands Команды.
+     */
+    public function __construct(
+        Application $application,
+        Command ...$commands
+    ) {
+        $this->application = $application;
+        $this->commands = $commands;
+    }
+
+    /**
+     * Инициализация команд.
+     *
+     * @return $this
+     */
+    public function init() : self
+    {
+        foreach ($this->commands as $command) {
+            $this->application->add($command);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Запуск команд.
+     *
+     * @throws Exception
+     */
+    public function run() : void
+    {
+        $this->application->run();
+    }
+
+    /**
+     * Добавить команды.
+     *
+     * @param mixed $commands Команды
+     *
+     * @return void
+     *
+     * @throws Exception
+     * @since 24.12.2020 Рефакторинг.
+     */
+    public function add(...$commands) : void
+    {
+        $result = [];
+
+        foreach ($commands as $command) {
+            $array = $command;
+            if ($command instanceof IteratorAggregate) {
+                $iterator = $command->getIterator();
+                $array = iterator_to_array($iterator);
+            }
+
+            $result[] = $array;
+        }
+
+        /** @psalm-suppress InvalidPropertyAssignmentValue */
+        $this->commands = array_merge($this->commands, $result);
+    }
+}
